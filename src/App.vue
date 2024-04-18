4f0461a42abc6e946e26e8f6b06e8bcd87eaa008
fengmengliu <kllfm@sina.com>
2024-04-18 19:06:20 +0800

    feat: 创建第一个简单的cesium应用

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
-<script setup lang='ts'>
-import { ref, reactive } from 'vue'
-import Cesium from 'cesium'
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+import * as Cesium from 'cesium'
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+// 存放workers,thirdParty,assets,widgets四个目录的位置 -- 要配置的否则加载不到相应资源
+window.CESIUM_BASE_URL = "/"
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer')
+})
 
 </script>
 
 <template>
-  <div>ceshi cesium</div>
+  <!-- cesium容器 -->
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
 </template>
 
 <style lang='scss' scoped>
-
+ *{
+  margin: 0;
+  padding: 0;
+ }
+ #cesiumContainer{
+  width: 100vw;
+  height: 100vh;
+ }
 </style>
