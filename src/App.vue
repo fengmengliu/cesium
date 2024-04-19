7d81278526ae85510982ed8cf20ca4681e142328
fengmengliu <kllfm@sina.com>
2024-04-19 21:31:40 +0800

    feat: 设置天空盒（即设置背景）

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, reactive, onMounted } from 'vue'
-import { Ion, Viewer, Camera, Rectangle } from 'cesium'
+import { Ion, Viewer, Camera, Rectangle, SkyBox } from 'cesium'
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 // 存放workers,thirdParty,assets,widgets四个目录的位置 -- 要配置的否则加载不到相应资源
@@ -21,8 +21,25 @@ Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(
 
 onMounted(() => {
   const viewer = new Viewer('cesiumContainer', {
-    // creditContainer: "cesiumContainer" // 通过此配置，可以隐藏logo，属性值为要隐藏logo的cesium容器
-    // infoBox: false, // 信息框控制台会报错，设置为false则不会再报错了
+    infoBox: false, // 信息框控制台会报错，设置为false则不会再报错了
+    geocoder: false, // 是否显示查看器右上角搜索框 -- 显示的时候可以快速搜索地区
+    homeButton: false, // 不显示HomeButton
+    sceneModePicker: false, // 控制场景查看器的显示模式(用来控制切换2D还是3D)
+    baseLayerPicker: false, // 是否显示图层选择器
+    navigationHelpButton: false, // 是否显示帮助按钮
+    animation: false, // 是否显示播放动画控制按钮
+    timeline: false, // 是否显示时间轴
+    fullscreenButton: false, // 是否显示全屏按钮
+    skyBox: new SkyBox({ // 设置天空盒 -- 即地球的默认背景图片
+      sources: {
+        positiveX: './texture/sky/px.jpg',
+        negativeX: './texture/sky/nx.jpg',
+        positiveY: './texture/sky/py.jpg',
+        negativeY: './texture/sky/ny.jpg',
+        positiveZ: './texture/sky/pz.jpg',
+        negativeZ: './texture/sky/nz.jpg'
+      }
+    })
   })
 
   // 也可以通过js的方式隐藏logo
